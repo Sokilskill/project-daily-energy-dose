f71a14939c2fb5fc0bc3b6a77c3ac9f747691f4f
Sokilskill <vprovorotov@gmail.com>
2024-01-11 21:44:02 +0200

    added Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-import FirstPage from 'pages/FirstPage/FirstPage';
-import SecondPage from 'pages/SecondPage/SecondPage';
-import HalfPage from 'pages/HalfPage/HalfPage';
+import ProductsPage from 'pages/ProductsPage/ProductsPage';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 import { AppWrapper } from './App.styled';
-
-const test = import.meta.env.VITE_API_TEST;
+import WelcomePage from './pages/WelcomePage/WelcomePage';
 
 function App() {
-  console.log(test);
   return (
     <AppWrapper>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route path="/first" element={<FirstPage />} />
-          <Route path="/second" element={<SecondPage />}>
-            <Route path=":half" element={<HalfPage />} />
-          </Route>
+          <Route path="/first" element={<WelcomePage />} />
+          <Route path="/second" element={<ProductsPage />}></Route>
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
