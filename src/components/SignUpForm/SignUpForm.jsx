f37729aa7972c27a01005b4ad5585d8814b8fa5a
Lyst67 <p_listopad@ukr.net>
2024-01-14 21:29:10 +0200

    signup_signin

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import css from './SignUpForm.module.css';
+
+export const SignUpForm = ({ onSubmit }) => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const form = evt.currentTarget;
+    const formData = {
+      name: form.name.value,
+      email: form.email.value,
+      password: form.password.value,
+    };
+    if (!formData) {
+      alert('Fill in the field, please!');
+      return;
+    }
+    onSubmit(formData);
+    form.reset();
+  };
+  return (
+    <>
+      <form className={css.form} onSubmit={handleSubmit} autoComplete="on">
+        <div className={css.inputs}>
+          <label className={css.form_label}>
+            <input
+              className={css.form_input}
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+            />
+          </label>
+          <label className={css.form_label}>
+            <input
+              className={css.form_input}
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </label>
+          <label className={css.form_label}>
+            <input
+              className={css.form_input}
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </label>
+        </div>
+
+        <button type="submit" className={css.form_btn}>
+          Sign Up
+        </button>
+      </form>
+    </>
+  );
+};
---
+++ src/redux/auth/auth-selectors.js
@@ -0,0 +1,3 @@
+export const selectIsLogedIn = (state) => state.auth.isLogedIn;
+export const selectUserName = (state) => state.auth.user.name;
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
