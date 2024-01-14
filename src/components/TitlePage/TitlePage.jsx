fd32942b41a45da5382fa6146b285952546cfa32
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-01-14 11:45:27 +0100

    profileSettings
    redux have written, just it need to make the corrections in operations with endpoints

--- src/components/TitlePage/TitlePage.jsx
+++ src/components/TitlePage/TitlePage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Title } from './TitlePage.styled';
 
-export const TitlePage = ({text}) => {
-    return (
-        <Title>{text}</Title>
-    );
+export const TitlePage = ({ text }) => {
+  return (
+  <Title>{text}</Title>
+  );
 };
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -4,8 +4,7 @@ import { TitlePage } from "../../components/TitlePage/TitlePage";
 const ProfilePage = () => {
     return (
         <div>
-          <h1>Hi</h1>
-            <TitlePage title="Profile Settings" />
+            <TitlePage text="Profile Settings" />
         </div>
     )
 };
