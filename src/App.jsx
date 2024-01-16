34cfa494df267f45bf94b820f8dc44b0081caa6e
Sokilskill <vprovorotov@gmail.com>
2024-01-16 02:09:58 +0200

    Update App.jsx
    added lasy (chunk)
    додана ліниве завантаження сторінок

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
-import ProductsPage from 'pages/ProductsPage/ProductsPage';
-import ErrorPage from 'pages/ErrorPage/ErrorPage';
-import WelcomePage from './pages/WelcomePage/WelcomePage';
+import { lazy } from 'react';
 import MainLayout from './components/MainLayout/MainLayout';
-import SignUpPage from './pages/SignUpPage/SignUpPage';
-import SignInPage from './pages/SignInPage/SignInPage';
-import DiaryPage from './pages/DiaryPage/DiaryPage';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
-import ExercisesPage from './pages/ExercisesPage/ExercisesPage.styled';
-import {store} from './redux/store'
-import { Provider } from 'react-redux';
 
 //неавторизованого користувача переадресовує на Welcome page, авторизованого
 //- на Diary page або Profile page(якщо на backendі відсутня інформація про параметри авторизованого користувача)
@@ -18,16 +8,22 @@ import { Provider } from 'react-redux';
 // та редірект на diary === коли користувач вже залогіненний заходить наприклад з
 // нової вкладки його не відправляє на сторінку Welcome
 
-// ліниве завантаження,
-// const WelcomePage = lazy(() => import('pages/WelcomePage/WelcomePage.jsx'));
+// додати перед Routes спінер-завантаження/лоадер поки не завантажиться сторінка -
 
 // авторизований / залогінений
 const isAuth = true;
-const Diary = lazy(() => import('./pages/DiaryPage/DiaryPage'));
+
+const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
+const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
+const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage'));
+const ProfilePage = lazy(() => import('./pages/ProfilePage/ProfilePage'));
+const DiaryPage = lazy(() => import('./pages/DiaryPage/DiaryPage'));
+const ProductsPage = lazy(() => import('./pages/ProductsPage/ProductsPage'));
+const ExercisesPage = lazy(() => import('./pages/ExercisesPage/ExercisesPage'));
+const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage'));
 
 function App() {
   return (
-    <Provider store={store}> {}
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<WelcomePage />} />
@@ -40,7 +36,6 @@ function App() {
         <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
-    </Provider>
   );
 }
 export default App;
