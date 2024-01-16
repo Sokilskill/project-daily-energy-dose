d6d840cbb50902e3503e97a6e04fd28361aea0e1
Sokilskill <126295444+Sokilskill@users.noreply.github.com>
2024-01-16 00:36:06 +0000

    Deploying to gh-pages from @ Sokilskill/project-daily-energy-dose@af6e264567f57618cd2c1f85601e6314171eabb0 🚀

---
+++ assets/ProductsPage-70047abb.js
@@ -0,0 +1,39 @@
+import{L as o,j as f}from"./index-5380e140.js";import{n as t}from"./emotion-styled.browser.esm-09526959.js";const e=t.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 500px;
+`,a=t.div`
+  text-align: center;
+`,x=t.h1`
+  color: #ffffff;
+  text-shadow:
+    0 0 5px #00baff,
+    0 0 10px #00baff,
+    0 0 20px #00baff,
+    0 0 40px #00baff,
+    0 0 80px #00baff;
+`,n=t(o)`
+  padding: 8px 16px;
+  color: #ffffff; /* White text color */
+  text-shadow:
+    0 0 5px #00baff,
+    0 0 10px #00baff,
+    0 0 20px #00baff,
+    0 0 40px #00baff,
+    0 0 80px #00baff;
+  transition:
+    color 0.3s,
+    text-shadow 0.3s;
+  display: block;
+  &:hover,
+  &:focus {
+    color: #00baff; /* Blue text color on hover/focus */
+    text-shadow:
+      0 0 5px #00baff,
+      0 0 10px #00baff,
+      0 0 20px #00baff,
+      0 0 40px #00baff,
+      0 0 80px #00baff;
+  }
+`,c=()=>f.jsx(e,{children:f.jsxs(a,{children:[f.jsx(x,{children:"Second Page"}),f.jsx(n,{to:"/second/5",children:"Half"})]})});export{c as default};
---
+++ assets/auth-selectors-142240d6.js
@@ -0,0 +1,1 @@
+const e=s=>s.auth.isLogedIn,t=s=>s.auth.user.bodyParams,a={getUserMetricData:t};export{a,e as s};
