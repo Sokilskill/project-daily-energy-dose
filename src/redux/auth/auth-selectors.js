a34f34411b300162dd7a2983cb33d84d1fbfde17
Lyst67 <p_listopad@ukr.net>
2024-01-17 18:45:15 +0200

    formik_fix_auth

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,5 +1,4 @@
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
-export const selectUserName = (state) => state.auth.user.name;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
 
 // Diary
@@ -7,6 +6,6 @@ const getUserMetricData = (state) => state.auth.user.bodyParams;
 
 const authSelectors = {
   getUserMetricData,
-}
+};
 
 export default authSelectors;
