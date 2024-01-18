621869e74a822737b132736ea2d5422ae3e0d310
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-01-18 15:32:35 +0100

    Styled_ProfilePage

--- src/helperComponents/DatePicker/DatePicker.jsx
+++ src/helperComponents/DatePicker/DatePicker.jsx
@@ -1,11 +1,33 @@
-import React, { useState, useRef } from 'react';
-import 'react-datepicker/dist/react-datepicker.css';
-import { StyledDatepicker } from './DatePicker.styled';
 
-const BirthdayPicker = () => {
+import React, { useState, useRef, useEffect } from 'react';
+import 'react-datepicker/dist/react-datepicker.css';
+import { StyledDatepicker, CustomDatePickerInput, StyledIcon} from './DatePicker.styled';
+import sprite from '../../assets/sprite.svg';
+
+const BirthdayPicker = ({
+  textSize,
+  textWeight,
+  iconColor,
+  textHeight,
+}) => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [popUpOpen, setPopUpOpen] = useState(true);
   const datePickerRef = useRef(null);
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setPopUpOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [wrapperRef]);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
@@ -13,39 +35,44 @@ const BirthdayPicker = () => {
 
   const handleSubmit = () => {
     console.log('Selected date:', selectedDate);
-    
-    // Close the date picker pop-up using the ref
+
     if (datePickerRef.current) {
-      datePickerRef.current.setOpen(false);
+      datePickerRef.current.setOpen(true);
     }
 
     setPopUpOpen(false);
   };
 
   return (
-    <div>
-      <StyledDatepicker
-        ref={datePickerRef}
-        selected={selectedDate}
-        onChange={handleDateChange}
-        dateFormat="dd.MM.yyyy"
-        placeholderText="00.00.0000"
-        showYearDropdown
-        showMonthDropdown
-        onSelect={handleSubmit}
-        popperModifiers={{
-          offset: {
-            enabled: true,
-            offset: '5px, 10px',
-          },
-          preventOverflow: {
-            enabled: true,
-            escapeWithReference: false,
-            boundariesElement: 'viewport',
-          },
-        }}
-      />
-    </div>
+    <div
+    ref={(node) => {
+      wrapperRef.current = node;
+    }}
+    style={{ position: 'relative' }}
+  >
+    <StyledDatepicker
+      ref={datePickerRef}
+      selected={selectedDate}
+      onChange={handleDateChange}
+      dateFormat="dd.MM.yyyy"
+      placeholderText="00.00.0000"
+      showYearDropdown
+      onSelect={handleSubmit}
+      required
+      customInput={
+        <CustomDatePickerInput className="profile"
+        >
+          <StyledIcon
+            onClick={handleSubmit}
+            stroke={iconColor}
+            style={{ position: 'absolute', right: '10px', top: '14px' }}
+          >
+            <use href={`${sprite}#icon-calendar`}></use>
+          </StyledIcon>
+        </CustomDatePickerInput>
+      }
+    />
+  </div>
   );
 };
 
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,17 +1,34 @@
-import React from "react";
-import { TitlePage } from "../../components/TitlePage/TitlePage";
-import { UserCard } from "../../components/UserCard/UserCard";
-import { ProfileContainer } from "./ProfilePage.styled";
-import { UserForm } from "../../components/UserForm/UserForm";
+import React from 'react';
+import { TitlePage } from '../../components/TitlePage/TitlePage';
+import { UserCard } from '../../components/UserCard/UserCard';
+import {
+  ProfileContainer,
+  ReversDiv,
+  TitleContainer,
+  UserCardContainer,
+  UserFormContainer,
+} from './ProfilePage.styled';
+import { UserForm } from '../../components/UserForm/UserForm';
 
 const ProfilePage = () => {
-    return (
-        <ProfileContainer>
-            <TitlePage text="Profile Settings" />
-            <UserCard/>
-            <UserForm/>
-        </ProfileContainer>
-    )
+  return (
+    
+      
+      <ProfileContainer>
+        <TitleContainer>
+        <TitlePage text="Profile Settings" />
+      </TitleContainer>
+        <ReversDiv>
+          <UserCardContainer>
+            <UserCard />
+          </UserCardContainer>
+
+          <UserFormContainer>
+            <UserForm />
+          </UserFormContainer>
+        </ReversDiv>
+      </ProfileContainer>
+  );
 };
 
 export default ProfilePage;
