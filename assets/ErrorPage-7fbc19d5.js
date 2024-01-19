e4e13af443af55410dfc0176baf22775e185628e
Sokilskill <126295444+Sokilskill@users.noreply.github.com>
2024-01-19 15:34:05 +0000

    Deploying to gh-pages from @ Sokilskill/project-daily-energy-dose@a6c7e9c6f48d83c3ef9db6e1c7f753d879da064f 🚀

--- assets/ErrorPage-7fbc19d5.js
+++ assets/ErrorPage-7fbc19d5.js
@@ -1,4 +1,4 @@
-import{n as f,j as t}from"./index-a84909d6.js";const e=f.div`
+import{n as f,j as t}from"./index-548df69e.js";const e=f.div`
   display: flex;
   justify-content: center;
   align-items: center;
--- assets/ExercisesPage-cfce879f.js
+++ assets/ExercisesPage-cfce879f.js
@@ -1,4 +1,4 @@
-import{n as s,N as a,a as r,u as c,r as d,j as e,z as p,A as x,B as l,O as m}from"./index-a84909d6.js";const g=s.ul`
+import{n as s,N as a,a as r,u as c,r as d,j as e,z as p,A as x,B as l,O as m}from"./index-548df69e.js";const g=s.ul`
   display: flex;
   gap: 28px;
 
