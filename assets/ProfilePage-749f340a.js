2a114b930385b58866b7c9af9d4997c68b2681bc
Sokilskill <126295444+Sokilskill@users.noreply.github.com>
2024-01-20 23:09:21 +0000

    Deploying to gh-pages from @ Sokilskill/project-daily-energy-dose@b57aed0952a432152ca8f5b65f482a550b8b72f0 🚀

---
+++ assets/ProfilePage-749f340a.js
@@ -0,0 +1,331 @@
+import{n as i,j as e,a as q,c as f,e as F,r as o,s as x,f as D,g as E,Q as W,h as z,i as L,k as R}from"./index-eea32f9b.js";import{c as N,a as S,e as u,F as M,b as I,d as l,E as n}from"./index.esm-e933f4b6.js";import"./hoist-non-react-statics.cjs-9c65089d.js";const T=i.h2`
+margin: 0;
+color: var(--white, #efede8);
+font-size: 24px;
+font-weight: 700;
+line-height: 28px;
+
+@media (min-width: 768px) {
+  font-size: 32px;
+  line-height: 1.4;
+}
+`,$=({text:a})=>e.jsx(T,{children:a}),B=i.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    width: 439px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 1440px) {
+    width: 439px;
+    margin: 0;
+  }
+`,V=i.div`
+  position: relative;
+  display: inline-block;
+  width: 90px;
+  height: 90px;
+  background-color: var(----background-color-avatar, rgba(48, 48, 48, 0.3));
+  border-radius: 100%;
+  margin-top: 40px;
+  border: 1px solid var(--orange, #e6533c);
+
+  @media (min-width: 768px) {
+    width: 150px;
+    height: 150px;
+  }
+`,H=i.div`
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+`,O=i.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  z-index: 2;
+  opacity: 0;
+`,Z=i.label`
+  cursor: pointer;
+  position: absolute;
+  top: 47%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 99;
+`,_=i.svg`
+  width: 24px;
+  height: 24px;
+
+  @media (min-width: 768px) {
+    width: 32px;
+    height: 32px;
+  }
+`,Q=i.img`
+  border-radius: 100%;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`,X=i.svg`
+  width: 62px;
+  height: 62px;
+  position: absolute;
+  top: 10%;
+  left: 15%;
+  fill: var(--default-avatar-fill, rgba(239, 237, 232, 0.1));
+
+  @media (min-width: 768px) {
+    width: 102px;
+    height: 102px;
+  }
+`,G=i.div`
+  width: 100%;
+  margin-top: 20px;
+  text-align: center;
+  gap: 8px;
+
+  @media (min-width: 768px) {
+    margin-top: 18px;
+  }
+`;i.p`
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.1;
+  color: var(--white: #efede8);
+
+  @media (min-width: 768px) {
+    font-size: 24px;
+    line-height: 1.2;
+  }
+`;const J=i.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.3;
+  color: var(--secondary-color-text, rgba(239, 237, 232, 0.5));
+`,K=i.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 14px;
+  margin: 40px 0 0 0;
+
+  @media (min-width: 768px) {
+    margin: 32px 0 0 0;
+    gap: 16px;
+  }
+`,Y=i.div`
+  display: flex;
+  width: 157px;
+  height: 96px;
+  flex-wrap: wrap;
+  background-color: var(--orange, #e6533c);
+  padding: 14px;
+  align-items: flex-start;
+  border-radius: 12px;
+  gap: 32px;
+
+  @media (min-width: 768px) {
+    width: 214px;
+    height: 108px;
+    gap: 28px;
+    flex-direction: column;
+  }
+`,ee=i.div`
+  display: flex;
+  gap: 8px;
+`,ie=i.svg`
+  width: 20px;
+  height: 20px;
+  fill: var(--icon-color, rgba(239, 137, 100, 1));
+`,te=i.p`
+  font-feature-settings: ('clig' off), ('liga' off);
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 16px;
+  color: var(--text-calorie-intake, rgba(239, 237, 232, 0.8)) @media
+    (min-width: 768px) {
+    line-height: 18px;
+  }
+`;i.p`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 1.1;
+  color: var(--white, #efede8);
+
+  @media (min-width: 768px) {
+    font-size: 24px;
+    line-height: 1.3;
+  }
+`;const ae=i.div`
+  display: flex;
+  width: 157px;
+  height: 96px;
+  flex-wrap: wrap;
+  background-color: var(--orange, #e6533c);
+  padding: 14px;
+  align-items: flex-start;
+  border-radius: 12px;
+  gap: 16px;
+
+  @media (min-width: 768px) {
+    width: 214px;
+    height: 108px;
+    gap: 28px;
+  }
+`,re=i.div`
+  display: flex;
+  gap: 8px;
+`,ne=i.svg`
+  width: 20px;
+  height: 20px;
+  fill: var(--icon-color, rgba(239, 137, 100, 1));
+`,se=i.p`
+  font-feature-settings: ('clig' off), ('liga' off);
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 16px;
+  color: var(--text-calorie-intake, rgba(239, 237, 232, 0.8)) @media
+    (min-width: 768px) {
+    line-height: 18px;
+  }
+`,le=i.p`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 1.1;
+  color: var(--white, #efede8);
+
+  @media (min-width: 768px) {
+    font-size: 24px;
+    line-height: 1.3;
+  }
+`,oe=i.div`
+  display: flex;
+  justify-content: center;
+  width: 335px;
+  margin: 40px 0 0 0;
+  gap: 8px;
+
+  @media (min-width: 768px) {
+    margin: 32px 0 0 0;
+    width: 439px;
+  }
+`,de=i.svg`
+  width: 24px;
+  height: 24px;
+`,he=i.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.3;
+  color: var(--secondary-text-color, #efede866);
+
+  @media (min-width: 768px) {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+`,ce=i.div`
+  width: 100%;
+  display: inline-flex;
+  justify-content: flex-end;
+  margin: 41px 40px 0 0;
+
+  @media (min-width: 768px) {
+    margin: 32px 0 0 0;
+  }
+
+  @media (min-width: 1440px) {
+    margin: 32px 0 0 0;
+  }
+`,C=a=>a.profile.profile.owner.name,pe=a=>a.profile.profile,xe=({time:a})=>{const w=q(),v=f(F),[b,j]=o.useState(v.avatarURL),[h,y]=o.useState(!0),[P,c]=o.useState({}),[k,m]=o.useState({}),[U,A]=o.useState(!1);f(C);const g=async p=>{const r=p.target.files[0];if(r)try{const s=new Blob([r]),t=URL.createObjectURL(s);j(t);const d=await w(E(r));console.log("newAvatar ProfilePage",r),console.log("data",d)}catch(s){console.error("Failed to create object URL:",s),W.error("Failed to update avatar")}finally{A(!1)}};return o.useEffect(()=>{h&&(c({borderRadius:"50%",width:"100%",height:"100%"}),y(!1))},[h]),o.useEffect(()=>{h||m({width:"90px",height:"90px"})},[h]),e.jsxs(B,{children:[e.jsxs(V,{children:[e.jsx(H,{children:e.jsx(O,{type:"file",id:"file-input",accept:"image/*",onChange:g})}),e.jsx(Z,{htmlFor:"file-input",children:e.jsx(_,{children:e.jsx("use",{href:`${x}#icon-check-mark-1`})})}),e.jsx("div",{children:b?e.jsx(Q,{src:b,alt:"Preview"}):e.jsx("div",{children:!v.avatarURL&&e.jsx(X,{children:e.jsx("use",{href:x+"#icon-gridicons_user"})})})})]}),e.jsx(G,{children:e.jsx(J,{children:"User"})}),e.jsxs(K,{children:[e.jsx(Y,{children:e.jsxs(ee,{children:[e.jsx(ie,{children:e.jsx("use",{href:x+"#icon-fluenit_food-24-filled"})}),e.jsx(te,{children:"Daily calorie intake"})]})}),e.jsxs(ae,{children:[e.jsxs(re,{children:[e.jsx(ne,{children:e.jsx("use",{href:x+"#icon-dumbbell"})}),e.jsx(se,{children:"Daily physical activity"})]}),e.jsxs(le,{children:[a,"0 min"]})]})]}),e.jsxs(oe,{children:[e.jsx("div",{children:e.jsxs(de,{children:[e.jsx("use",{href:x+"#icon-Ellipse-1",style:{fill:"rgba(239, 160, 130, 1)",width:"100%",height:"100%",position:"relative"}}),e.jsx("use",{href:x+"#icon-tabler_exclamation-mark",style:{fill:"rgba(239, 237, 232, 1)",position:"absolute",top:"50%",left:"50%",width:"100%",height:"100%"}})]})}),e.jsx(he,{children:"We understand that each individual is unique, so the entire approach to diet is relative and tailored to your unique body and goals."})]}),e.jsx(ce,{children:e.jsx(D,{})})]})},me=i.div`
+  padding-top: 40px;
+  padding-bottom: 40px;
+
+  @media (min-width: 768px) {
+    padding-top: 72px;
+    padding-bottom: 72px;
+  }
+`,ge=i.div`
+  // display: flex;
+  // padding: 0 20px;
+  // width: 100%;
+  @media (min-width: 768px) {
+    // margin: 72px 0 0 0;
+    // padding: 0 32px;
+  }
+
+  @media (min-width: 1440px) {
+    // width: 1248px;
+    // padding: 0 96px;
+  }
+`,ue=i.div`
+  // @media (min-width: 1440px) {
+  //   display: flex;
+  // justify-content: center;
+  // width: 100%;
+
+  //   flex-direction: row-reverse;
+  //   justify-content: space-between;
+  //   max-width: 100%;
+
+  // }
+
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding-top: 20px;
+  position: relative;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 60%;
+    transform: translateX(-50%);
+    width: 1px;
+    background-color: rgba(239, 237, 232, 0.2);
+    display: none; /* Початково приховуємо лінію */
+  }
+
+  @media (min-width: 1440px) {
+    &:before {
+      display: block; /* Показуємо лінію на десктопі */
+    }
+  }
+
+  @media (max-width: 1439px) {
+    flex-direction: column;
+  }
+`;i.div`
+  @media (min-width: 1440px) {
+    // width: 704px;
+  }
+`;i.div`
+@media (min-width: 1440px) {
+//  width: 445px;
+ &::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: -8px;
+  height: 100%;
+  width: 1px;
+  background-color: var(--border, #efede84d);
+}
+`;const fe=N().shape({name:S().matches(/^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$/,"Invalid name format").required("Name is required"),email:S().email("Invalid email address").required("Email is required"),sex:S().required("Choose one of these fields"),height:u().label("Height").min(150,"Must be a height min 150(sm)").max(230,"Must be a height max 230(sm)").required("Height is required"),desiredWeight:u().label("Desired Weight").min(35,"Must be a weight min 35(kg)").max(300,"Must be a number from 35 to 300").required("Desired Weight is required"),currentWeight:u().label("Weight").min(35,"Must be a weight min 35(kg)").max(300,"Must be a number from 35 to 300").required("Weight is required"),blood:u().label("blood").required("Choose one of these fields"),levelActivity:u().label("levelActivity").required("Choose one of these fields")}),ve=[{label:"1",value:1},{label:"2",value:2},{label:"3",value:3},{label:"4",value:4}],be=[{label:"Male",value:"male"},{label:"Female",value:"female"}],we=[{id:1,label:"Sedentary lifestyle (little or no physical activity)",value:1},{id:2,label:"Light activity (light exercises/sports 1-3 days per week)",value:2},{id:3,label:"Moderately active (moderate exercises/sports 3-5 days per week)",value:3},{id:4,label:"Very active (intense exercises/sports 6-7 days per week)",value:4},{id:5,label:"Extremely active (very strenuous exercises/sports and physical work)",value:5}],je=()=>{const a=q(),{height:w,currentWeight:v,desiredWeight:b,birthday:j,blood:h,sex:y,levelActivity:P}=f(pe),c=f(C),k=f(F);o.useEffect(()=>{c&&a(z())},[a,c]);const m=c||k.name;console.log("currentName",m);const U={name:m||"",email:k.email||"",height:w||"",currentWeight:v||"",desiredWeight:b||"",birthday:j||0,blood:h||0,sex:y||"",levelActivity:P||1},A=async g=>{try{console.log("DATA",g);const{name:p,email:r,...s}=g,t=await a(L({name:p})),d=await a(R(s));t.meta.requestStatus==="fulfilled"&&d.meta.requestStatus==="fulfilled"?(await a(z()),W.success("Дані оброблюються, створюється план тренувань")):console.log("Помилка оновлення даних")}catch(p){W.error("An error occurred while updating the profile"),console.error("Error updating profile:",p)}};return m&&e.jsx(M,{initialValues:U,validationSchema:fe,onSubmit:A,children:({errors:g,touched:p,values:r,setFieldValue:s})=>e.jsxs(I,{children:[e.jsxs("div",{children:[e.jsx("label",{htmlFor:"name",children:"Name"}),e.jsx(l,{type:"text",id:"name",name:"name",value:r.name}),e.jsx(n,{name:"name",component:"div"})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"email",children:"Email"}),e.jsx(l,{type:"text",id:"email",name:"email"}),e.jsx(n,{name:"email",component:"div"})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"height",children:"Height"}),e.jsx(l,{type:"number",id:"height",name:"height"}),e.jsx(n,{name:"height",component:"div"})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"currentWeight",children:"currentWeight"}),e.jsx(l,{type:"number",id:"currentWeight",name:"currentWeight"}),e.jsx(n,{name:"currentWeight",component:"div"})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"desiredWeight",children:"desiredWeight"}),e.jsx(l,{type:"number",id:"desiredWeight",name:"desiredWeight"}),e.jsx(n,{name:"desiredWeight",component:"div"})]}),e.jsxs("div",{children:[e.jsx("label",{htmlFor:"birthday",children:"Birthday"}),e.jsx("input",{type:"number",id:"birthday",name:"birthday"}),e.jsx(n,{name:"birthday",component:"div"})]}),e.jsxs("div",{children:[e.jsx("label",{children:"Blood"}),ve.map((t,d)=>e.jsxs("label",{children:[e.jsx(l,{type:"radio",name:"blood",value:t.value,checked:r.blood===t.value,onChange:()=>s("blood",t.value)}),t.label]},d)),e.jsx(n,{name:"blood",component:"div"})]}),e.jsxs("div",{children:[be.map((t,d)=>e.jsxs("label",{children:[e.jsx(l,{type:"radio",name:"sex",value:t.value}),t.label]},d)),e.jsx(n,{name:"sex",component:"div"})]}),e.jsxs("div",{children:[we.map(t=>e.jsxs("label",{children:[e.jsx(l,{type:"radio",name:"levelActivity",value:t.value,checked:r.levelActivity===t.value,onChange:()=>s("levelActivity",t.value)}),t.label]},t.id)),e.jsx(n,{name:"levelActivity",component:"div"})]}),e.jsx("button",{type:"submit",children:"Save"})]})})},Se=()=>e.jsxs(me,{className:"container",children:[e.jsx(ge,{children:e.jsx($,{text:"Profile Settings"})}),e.jsxs(ue,{children:[e.jsx(xe,{}),e.jsx(je,{})]})]});export{Se as default};
