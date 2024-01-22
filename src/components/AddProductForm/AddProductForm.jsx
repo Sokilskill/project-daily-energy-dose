81f0a59f6a9292fe56a2e6c517e0a125cd84fcf3
Marianna Demchenko <mdemko2012@gmail.comm>
2024-01-22 19:44:27 +0100

    add modal suc

--- src/components/AddProductForm/AddProductForm.jsx
+++ src/components/AddProductForm/AddProductForm.jsx
@@ -1,10 +1,53 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { MdClose } from "react-icons/md";
-
+import axios from 'axios';
 import { Backdrop, ModalContainer, Close, Form, Input, Inputs, InputWrapper, InputContainer,Placeholder, Text, Button1,Button2, Buttons} from "./AddProductForm.stiled";
+import AddProductSuccess from "../AddProductSuccess/AddProductSuccess";
 
-const AddProductForm = ({onClose, open}) => {
-  const handleKeyDown = useCallback((event) => {
+const AddProductForm = ({onClose, open, title,  onAddSuccess, onError, userId}) => {
+
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState(1);
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+
+ const calculateCalories = (amount) => {
+   
+    return (amount * 100) / 100; 
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+
+    const calories = calculateCalories(amount);
+
+    const dataForm = {
+      product,
+      amount,
+      calories,
+      userId, 
+    };
+
+    try {
+      await axios.post('/add-entry', dataForm);
+
+      setShowSuccessModal(true);
+      setProduct('');
+      setAmount(1);
+
+      // Викликайте onAddSuccess
+      onAddSuccess();
+    } catch (error) {
+      // Обробка помилок відправки на бекенд
+      console.error('Помилка відправки на бекенд:', error);
+      // Викликайте функцію onError, щоб показати повідомлення про помилку
+      onError('Помилка відправки на бекенд. Спробуйте ще раз.');
+    }
+  }
+
+  const handleSuccessModalClose = () => {
+    setShowSuccessModal(false);
+  };
+    const handleKeyDown = useCallback((event) => {
     if (event.key === 'Escape') {
       onClose();
     }
@@ -25,29 +68,40 @@ const AddProductForm = ({onClose, open}) => {
             <Close>
                <MdClose onClick={onClose} size='22px' />
             </Close>
-        
-            <Form name="addProductForm" action="#">
+            <Form name="addProductForm" action="#" onSubmit={handleSubmit}>
               <Inputs>
                 <Input 
                   id="product" 
                   type="text" 
                   name="product" 
-                  placeholder="product">
+                  disabled
+                  value={title}
+                  placeholder="product"
+                  >
                 </Input>
                 <InputContainer>
                   <InputWrapper>
-                    <Input type="text" />
+                    <Input 
+                      type="text"
+                    />
                     <Placeholder>grams</Placeholder>
                   </InputWrapper>
                 </InputContainer>
               </Inputs>
               <Text>Calories:</Text>
               <Buttons>
-                <Button1>Add to diary</Button1>
-                <Button2>Cancel</Button2>
+                <Button1 type="submit" >Add to diary</Button1>
+                <Button2 type="button" onClick={onClose}>Cancel</Button2>
               </Buttons>
+            </Form>
+            {showSuccessModal && (
+              <AddProductSuccess
               
-            </Form> 
+              />
+              <div>
+                <p>Додано продукт успішно!</p>
+                <button onClick={handleSuccessModalClose}>Закрити</button>
+              </div> 
           </ModalContainer>
         </Backdrop>
         
