a39557885d2cd6e064f9ee72bc5db8b43a80c22b
Sokilskill <vprovorotov@gmail.com>
2024-01-22 18:41:46 +0200

    Update MainLayout.jsx
    логіка хедер не відображається на errorPage

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,12 +1,31 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import Loader from '../Loader/Loader';
 
 const MainLayout = () => {
+  const location = useLocation();
+
+  // Define an array of valid paths where the header should be displayed
+  const validPaths = [
+    '/signup',
+    '/signin',
+    '/profile',
+    '/diary',
+    '/products',
+    '/exercises',
+    '/exercises/bodyParts',
+    '/exercises/muscles',
+    '/exercises/equipment',
+    '/exercises/list',
+  ];
+
+  // Check if the current location is in the valid paths array
+  const isHeaderVisible = validPaths.includes(location.pathname);
+
   return (
     <>
-      <Header />
+      {isHeaderVisible && <Header />}
       <main>
         <Suspense fallback={<Loader />}>
           <Outlet />
