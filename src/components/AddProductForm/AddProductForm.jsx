9b48580928a00809cb6775fae0987ef269e92bbc
Chapelk1 <klepach154789@gmail.com>
2024-01-23 06:08:14 +0200

    +

--- src/components/AddProductForm/AddProductForm.jsx
+++ src/components/AddProductForm/AddProductForm.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback, useState } from 'react';
 import { MdClose } from "react-icons/md";
 
-import { Backdrop, ModalContainer, Close, Form, Input, Inputs, InputWrapper, InputContainer,Placeholder, Text, Button1,Button2, Buttons} from "./AddProductForm.stiled";
+import { Backdrop, ModalContainer, Close, Form, Input, Inputs, InputWrapper, InputContainer,Placeholder, Text, Button1,Button2, Buttons, ImitationInput,TextInInput} from "./AddProductForm.stiled";
 
-const AddProductForm = ({onClose, open}) => {
+const AddProductForm = ({ onClose, open, title, calories }) => {
+  const [actualCalories, setactualCalories] = useState(0);
+  const [gram, setGram] = useState('');
+
+
   const handleKeyDown = useCallback((event) => {
     if (event.key === 'Escape') {
       onClose();
@@ -18,36 +22,50 @@ const AddProductForm = ({onClose, open}) => {
     };
   }, [handleKeyDown]); 
 
+  const handlerInputChange = (e) => {
+    const actCalories = ((Number(e.target.value) * calories) / 100)
+    
+    if (actCalories) {
+      setactualCalories(actCalories);
+    }
+    setGram(e.target.value);
+  }
+
+  const handlerSubmit = (e) => {
+    e.preventDefault()
+    onClose();
+    return 
+  }
+  const normalizedTitle = `${title[0].toUpperCase() + title.slice(1,111)}`;
   return (
     <>
-      <Backdrop onClick={onClose} style={{display: open ? 'flex' : 'none'}}>
+      <Backdrop onClick={onClose} >
           <ModalContainer onClick={event => event.stopPropagation()}>
             <Close>
                <MdClose onClick={onClose} size='22px' />
             </Close>
         
-            <Form name="addProductForm" action="#">
+            <Form name="addProductForm" onSubmit={handlerSubmit}>
               <Inputs>
-                <Input 
-                  id="product" 
-                  type="text" 
-                  name="product" 
-                  placeholder="product">
-                </Input>
+              <ImitationInput>
+                <TextInInput>
+                  {normalizedTitle}
+                </TextInInput>
+                </ImitationInput>
                 <InputContainer>
                   <InputWrapper>
-                    <Input type="text" />
+                    <Input type="text" value={gram} onChange={handlerInputChange} required pattern='[1-9]{1,5}'/>
                     <Placeholder>grams</Placeholder>
                   </InputWrapper>
                 </InputContainer>
               </Inputs>
-              <Text>Calories:</Text>
-              <Buttons>
-                <Button1>Add to diary</Button1>
-                <Button2>Cancel</Button2>
+            <Text>{`Calories: ${actualCalories}`}</Text>  
+            <Buttons>
+                <Button1 type='submit'>Add to diary</Button1>
+                <Button2 type='button' onClick={onClose}>Cancel</Button2>
               </Buttons>
-              
-            </Form> 
+          </Form> 
+          
           </ModalContainer>
         </Backdrop>
         
