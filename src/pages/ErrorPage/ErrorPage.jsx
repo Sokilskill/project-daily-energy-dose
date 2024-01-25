5b9aabd5f33f53df2aa9a0cbd34e9b900fe98829
Serhii <126295444+Sokilskill@users.noreply.github.com>
2024-01-25 12:07:01 +0200

    Revert "Welcome bgd"

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -10,7 +10,6 @@ import {
   SectionContainer,
   LogoWrap,
   Container,
-  ErrorSection,
 } from './ErrorPage.styled';
 import { useDispatch } from 'react-redux';
 import { setErrorPage } from '../../redux/errorPage/err-slice';
@@ -34,33 +33,32 @@ const ErrorPage = () => {
 
   return (
     <>
-      <ErrorSection
+      <SectionWrapper
         bgi={[bg_mod_1, bg_mod_2, bg_tab_1, bg_tab_2, bg_desk_1, bg_desk_2]}
+        className="container section-wrapper"
       >
-        <SectionWrapper className="container section-wrapper">
-          <BackgroundColor>
-            <Container className="container">
-              <LogoWrap>
-                <Logo colorWhite="true" />
-              </LogoWrap>
-              <SectionContainer>
-                <ContentBox>
-                  <Title>404</Title>
-                  <Text>
-                    Sorry, you have reached a page that we could not find. It
-                    seems that you are lost among the numbers and letters of our
-                    virtual space. Perhaps this page went on vacation or decided
-                    to disappear into another dimension. We apologize for this
-                    inconvenience.
-                  </Text>
+        <BackgroundColor>
+          <Container className="container">
+            <LogoWrap>
+              <Logo colorWhite="true" />
+            </LogoWrap>
+            <SectionContainer>
+              <ContentBox>
+                <Title>404</Title>
+                <Text>
+                  Sorry, you have reached a page that we could not find. It
+                  seems that you are lost among the numbers and letters of our
+                  virtual space. Perhaps this page went on vacation or decided
+                  to disappear into another dimension. We apologize for this
+                  inconvenience.
+                </Text>
 
-                  <Link to="/">Go Home</Link>
-                </ContentBox>
-              </SectionContainer>
-            </Container>
-          </BackgroundColor>
-        </SectionWrapper>
-      </ErrorSection>
+                <Link to="/">Go Home</Link>
+              </ContentBox>
+            </SectionContainer>
+          </Container>
+        </BackgroundColor>
+      </SectionWrapper>
     </>
   );
 };
