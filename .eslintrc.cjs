b1424970ff663e61969eb5a50f8fc218deb9e0f5
sokilskills <vprovorotov@amil.com>
2025-04-25 09:52:51 +0300

    update: update Eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,29 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: '2022',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'no-console': 'error',
+    'no-unused-vars': 'error',
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
     'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
