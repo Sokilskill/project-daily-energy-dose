19a3775f701e8413f19a692ed3b2573332662aae
Sokilskill <126295444+Sokilskill@users.noreply.github.com>
2025-04-27 10:03:05 +0300

    Revert "update: update Eslint config"

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,18 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node: true },
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:jsx-a11y/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: {
-    ecmaVersion: '2022',
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true },
-  },
-  settings: {
-    react: { version: '18.2' },
-    'import/resolver': {
-      vite: {
-        viteConfig: './vite.config.js',
-      },
-    },
-  },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'no-console': 'error',
-    'no-unused-vars': 'error',
-    'import/no-unresolved': 'error',
-    'import/named': 'error',
     'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
